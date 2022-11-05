80e5d6cd9f62fe202c4e8e3fadda262b237b991c
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-05 00:39:25 -0500

    Add main.py file

---
+++ src/python/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""main.py file form which all other function, helper,
+class files can be called from.
+"""
+
+
+def main(argv):
+    pass
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main(sys.argv))
+
+
