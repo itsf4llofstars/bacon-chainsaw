31e136c2783d7e211add8c34c5b8ef3ce2ad0200
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-06 21:57:09 -0600

    Add initial craps.py file
    Write CrapsTable class and attrbutes

---
+++ craps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""A roll of the dice simulator show outcomes
+of dice used in a standard Vegas style craps game
+"""
+import random
+
+
+class CrapsTable:
+    def __init__(self, k_index: int = 1000):
+        self.dice1 = 0
+        self.dice2 = 0
+        self.outcome = [1, 2, 3, 4, 5, 6]
+        self.outcomes = random.choices(
+            [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+            [1 / 36, 1 / 18, 1 / 12, 1 / 9, 5 / 36, 1 / 6, 5 / 36, 1 / 9, 1 / 12, 1 / 18, 1 / 36],
+            k=k_index)
+        self.snake_eyes = 0
+        self.ace_duece = 0
+        self.come_win = 0
+        self.yo = 0
+        self.craps = 0
+        self.come_loss = 0
+        self.come_seven = 0
+        self.points = 0
+        self.point = 0
