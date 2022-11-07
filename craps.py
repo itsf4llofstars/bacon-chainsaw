20d1c2961e17d1f7898f5217f0bdfb0218aa5cf5
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-06 22:01:36 -0600

    Write dice roll and total methods

--- craps.py
+++ craps.py
@@ -9,11 +9,13 @@ class CrapsTable:
     def __init__(self, k_index: int = 1000):
         self.dice1 = 0
         self.dice2 = 0
+        self.total = 0
         self.outcome = [1, 2, 3, 4, 5, 6]
-        self.outcomes = random.choices(
+        self.dice_outcomes = random.choices(
             [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             [1 / 36, 1 / 18, 1 / 12, 1 / 9, 5 / 36, 1 / 6, 5 / 36, 1 / 9, 1 / 12, 1 / 18, 1 / 36],
             k=k_index)
+        self.outcomes = []
         self.snake_eyes = 0
         self.ace_duece = 0
         self.come_win = 0
@@ -23,3 +25,11 @@ class CrapsTable:
         self.come_seven = 0
         self.points = 0
         self.point = 0
+
+    def roll_dice(self):
+        self.dice1 = random.choice(self.outcome)
+        self.dice2 = random.choice(self.outcome)
+
+    def total_dice(self):
+        self.total = self.dice1 + self.dice2
+
