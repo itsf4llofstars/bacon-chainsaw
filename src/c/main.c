0adefda173f379986d5d3c0aaa85e157c1119bb3
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-10 00:19:57 -0600

    Add car struct

--- src/c/main.c
+++ src/c/main.c
@@ -1,11 +1,18 @@
+#include <stdio.h>
 /**
  * Just some C code for exploring GitHub
  * */
-#include <stdio.h>
+
+typedef struct
+{
+    const char *make;
+    int year;
+} car; 
 
 int main(int argc, char *argv[])
 {
-    printf("Hello, world!\n");
+    car aCar = {"Chevy", 2021};
+    printf("I own a %d %s Volt.\n", aCar.year, aCar.make);
 
     puts("");
     return 0;
