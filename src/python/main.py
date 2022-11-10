0e2e415d9e5e289ce7647502ee701e022d6ae2bd
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-10 13:18:15 -0600

    Move get_year function to randoms.py

--- src/python/main.py
+++ src/python/main.py
@@ -2,23 +2,6 @@
 """main.py file form which all other function, helper,
 class files can be called from.
 """
-import randoms
-
-
-def get_year(low, high):
-    """Calls the generate_rand_int function in
-    randoms.py, returns a year and prints it to
-    the terminal.
-
-    parameters:
-        low (int): The lowest year for random choice.
-        high (int): The highest year for random choice.
-
-    returns:
-        (int): A random year between low, high inclusive.
-    """
-    return randoms.generate_rand_int(high, low)
-
 
 def main(argv):
     random_year = randoms.generate_rand_int(2022, 1941)
--- src/python/randoms.py
+++ src/python/randoms.py
@@ -17,6 +17,21 @@ def generate_rand_int(high: int, low: int = 1) -> int:
     return random.randint(low, high)
 
 
+def get_year(low, high):
+    """Calls the generate_rand_int function in
+    randoms.py, returns a year and prints it to
+    the terminal.
+
+    parameters:
+        low (int): The lowest year for random choice.
+        high (int): The highest year for random choice.
+
+    returns:
+        (int): A random year between low, high inclusive.
+    """
+    return randoms.generate_rand_int(high, low)
+
+
 def main(argv):
     a_random_int = generate_rand_int(100)
     print(f'{a_random_int = }')
