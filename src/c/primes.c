6cbdaab1bf87f534887ac1d217610a8011ad2173
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-12 00:43:58 -0600

    Add initial check prime number

--- src/c/primes.c
+++ src/c/primes.c
@@ -18,8 +18,30 @@ void printPrimes(int start, int stop) {
     }
 }
 
+int isItPrime(int isPrime) {
+    int chkPrime = 1;
+    int halfIsPrime = isPrime / 2;
+
+    while (1)
+    {
+        ++chkPrime;
+
+        if (chkPrime >= halfIsPrime)
+            return 1;
+        else if (isPrime % chkPrime == 0)
+            break; 
+    
+        ++chkPrime;
+    }
+
+    return 0;
+}
+
 int main(int argc, char **argv[]) {
-    printPrimes(2, 15);
+    // printPrimes(2, 15);
+    if (isItPrime(47)) {
+        printf("47 is prime\n");
+    }
 
     return 0;
 }
