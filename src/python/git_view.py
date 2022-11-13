c75bec814900163361197c28024c28671eefcdbb
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-11-13 13:39:05 -0600

    Reset git_view.py
    Reset to initial code prior to viewing interaction with PyCharm

--- src/python/git_view.py
+++ src/python/git_view.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python3
-"""Python file to watch how things interact with PyCharm
-Git and GitHub
-"""
+"""git_view.py"""
 
 
-def is_it_prime(number: int) -> bool:
-    """Returns True if the passed number is prime."""
-    chk_prime: int = 1
-
-    while chk_prime <= (number // 2):
-        chk_prime += 1
-
-        if not number % chk_prime:
-            return False
-
-    return True
+def main() -> None:
+    pass
 
 
 if __name__ == "__main__":
-    n: int = 66
-
-    print(f"{n} is ", end="")
-
-    if not is_it_prime(n):
-        print("not ", end="")
-    
-    print("prime.")
+    main()
