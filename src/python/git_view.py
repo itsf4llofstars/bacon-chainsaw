8035b724581bbc62c455579e1eaba4e905794c1d
itsf4llofstars <90528743+itsf4llofstars@users.noreply.github.com>
2022-11-13 13:10:57 -0600

    Change single quotes to double quotes

--- src/python/git_view.py
+++ src/python/git_view.py
@@ -17,12 +17,12 @@ def is_it_prime(number: int) -> bool:
     return True
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     n: int = 13
 
-    printf(f'{n} is ', end='')
+    printf(f"{n} is ", end="")
 
     if not is_it_prime(n):
-        print('not ', end='')
+        print("not ", end="")
     
-    print('prime.')
+    print("prime.")
