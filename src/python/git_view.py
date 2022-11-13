7c22e85f191d98964ec9f684561cdffe18c3a099
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-13 12:16:43 -0600

    Add add two numbers function

--- src/python/git_view.py
+++ src/python/git_view.py
@@ -2,10 +2,27 @@
 """DOC"""
 
 
+def add_two_nums(a: int, b: int = 47) -> int:
+    """
+    parameters:
+        a [int]: Parameter to be added.
+        b [int]: Parmeter to be added. (optional) deafulted to 47
+
+    return:
+        [int]: b added to a
+    """
+    c: int = a + b
+    return c
+
+
 def main(argv, *args, **kwargs) -> None:
     """main function"""
-    if not argv[0] or not len(argv):
-        sys.exit()
+    print(add_two_nums(int(argv[1])))
+    print(add_two_nums(int(argv[1]), int(argv[2])))
+
+    x: int = 20
+    y: int = 30
+    print(add_two_nums(x, y))
 
 
 if __name__ == '__main__':
