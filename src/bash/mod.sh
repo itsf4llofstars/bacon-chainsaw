c27339ac8c9e86c85b0586c0c53b140c5bcb67d8
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-15 23:07:39 -0600

    Added first test of mod.sh
    Moved mod.sh to src/bash

--- src/bash/mod.sh
+++ src/bash/mod.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # A script symbolic link in your path to more easly
 # change a file's permessions to executable
+
+FILENAME="$1"
+EXECUTE=755
+
+if [ "$FILENAME" ]; then
+    printf "OK: %s %s" "$FILENAME" "$EXECUTE"
+fi
