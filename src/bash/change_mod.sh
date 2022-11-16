07f8e3bbd0d7365dbd432e9629fbc6a72133328d
itsf4llofstars <irooted4hal@mailfence.com>
2022-11-15 23:21:24 -0600

    Gramatical change in documentation

--- src/bash/change_mod.sh
+++ src/bash/change_mod.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-# A script symbolic link in your path to more easly
-# change a file's permessions to executable
+# A script that the user can create a symbolic
+# link in their path to more easly change
+# a file's permessions to executable
 
 FILENAME="$1"
 EXECUTE=755
