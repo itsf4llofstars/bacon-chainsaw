8e6d43f6ede089e41e22eefe640eb016e7e03ea9
itsf4llofstars <irooted4hal@mailfence.com>
2022-12-10 01:17:45 -0600

    Setup archiver file

---
+++ src/python/py_archiver.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Python3 script that creates an archive (tar file) of user wanted files
+"""
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    import sys
+
+    sys.exit(main())
