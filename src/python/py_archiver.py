edf8c36a579d3ca2995a1e9478ae14d7039cd0b1
itsf4llofstars <irooted4hal@mailfence.com>
2022-12-10 02:01:57 -0600

    Add variable names to main function

--- src/python/py_archiver.py
+++ src/python/py_archiver.py
@@ -9,9 +9,9 @@ PATH_TO_FILES: str = os.path.expanduser('~')
 
 """The below list should be populated by the user. If a file is not
 int the path, /home/$USER then the path to the file from, /home/$USER
-should be used. See FILES_LIST[0], ./config/nvim/init.vim for an example.
+should be used. See FILES_TUPLE[0], ./config/nvim/init.vim for an example.
 """
-FILES_LIST = (
+FILES_TUPLE = (
     ".config/nvim/init.vim",
     ".bashrc",
     ".bash_aliases",
@@ -26,13 +26,26 @@ def main():
     print(PATH_TO_FILES)
 
     # Check files list
-    for filename in FILES_LIST:
+    for filename in FILES_TUPLE:
         print(os.path.join(PATH_TO_FILES, filename))
     print()
 
-    base_name: str = os.path.join(PATH_TO_FILES, "foobar", "test_tar")
+    # The relative directory from /home/$USER
+    relative_dir: str = "foobar"
+
+    # The archive filename without the tar extension
+    tar_name: str = "test_tar"
+
+    # The directory where the tar file will be created
+    base_name: str = os.path.join(PATH_TO_FILES, relative_dir, tar_name)
+
+    # Archive type
+    tar_type: str = "tar"
+
+    # The directory and files that will be archived
     root_dir: str = os.path.join(PATH_TO_FILES, "ed")
-    shutil.make_archive(base_name, "tar", root_dir)
+
+    shutil.make_archive(base_name, tar_type, root_dir)
 
 
 if __name__ == "__main__":
