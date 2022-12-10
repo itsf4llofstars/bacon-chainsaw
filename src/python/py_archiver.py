1e59910b2aa674df3f62a1f717ef9d51728399c1
itsf4llofstars <irooted4hal@mailfence.com>
2022-12-10 01:29:44 -0600

    Add files_list

--- src/python/py_archiver.py
+++ src/python/py_archiver.py
@@ -3,6 +3,21 @@
 """
 import os
 
+path_to_files: str = os.path.expanduser('~')
+
+"""The below list shoudl be populated by the user. If a file is not
+int the path, /home/$USER then the path to the file from, /home/$USER
+should be used. See files_list[0], ./config/nvim/init.vim for an example.
+"""
+files_list = [
+    ".config/nvim/init.vim",
+    ".bashrc",
+    ".bash_aliases",
+    ".tmux.conf",
+    ".gitconfig",
+    ".nanorc"
+]
+
 
 def main():
     pass
